141cf9e4f34869505158b955958fb8de09024ef1
Omar Alhori <omaralhoori@gmail.com>
2025-12-09 20:57:43 +0100

    reconfig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    ppr: 'incremental',
     serverActions: {
       bodySizeLimit: '5mb',
     },
